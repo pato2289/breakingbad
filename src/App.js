4eadaa4f1a6867f6123321883571108af695b016
patobordenave <patriciofbordenave@gmail.com>
2019-12-04 15:40:36 -0300

    commit inicial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
 
-function App() {
+function Frase ({frase}) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="frase">
+      <h1>{frase.quote}</h1>
+      <p>- {frase.author}</p>
     </div>
-  );
+  )
 }
 
+const App = () => {
+
+const [frase, obtenerFrase] = useState({})
+
+
+//tener un metodo aparte, ya que en useEffect no se puede hacer, solo llamar a la funcion
+const consultarAPI = async () => {
+    const resultado = await axios('https://breaking-bad-quotes.herokuapp.com/v1/quotes');
+
+    //agregar el resultado del api al state (similiar a setState)
+    obtenerFrase(resultado.data[0]);
+}
+
+//consulta a una rest API con axios
+useEffect(
+  () => {
+    consultarAPI()
+  }, [] //este array vacio hace que no llame muchas veces, dependencias.
+)
+
+console.log(frase) //frase = this.state
+
+  return (
+    <div className="contenedor">
+      <Frase 
+        frase={frase}
+      />
+      <button
+        onClick={consultarAPI}
+      >Generar Nueva</button>
+    </div>
+   );
+}
+ 
 export default App;
