ea6f6885c40164374b3a821e4ef87e79de133007
patobordenave <patriciofbordenave@gmail.com>
2019-12-04 15:45:22 -0300

    Updates

---
+++ precache-manifest.4922e5205b731876e20272f7663c7140.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00bd871ab69322487eacbf6298fac8cc",
+    "url": "/breakingbad/index.html"
+  },
+  {
+    "revision": "dad8b1b0b4bdb408b181",
+    "url": "/breakingbad/static/css/main.5037643a.chunk.css"
+  },
+  {
+    "revision": "9d4213d4c51174377b5a",
+    "url": "/breakingbad/static/js/2.6a31fa13.chunk.js"
+  },
+  {
+    "revision": "dad8b1b0b4bdb408b181",
+    "url": "/breakingbad/static/js/main.17947c1b.chunk.js"
+  },
+  {
+    "revision": "40efcd6e014a95c998a0",
+    "url": "/breakingbad/static/js/runtime-main.14514d8f.js"
+  },
+  {
+    "revision": "427a4adc3c0a0dec192d14617aaa6c4c",
+    "url": "/breakingbad/static/media/logo.427a4adc.svg"
+  }
+]);
